7fe9283b94168e89c923131380e6f5753574a4f5
Sundaram04 <145194021+Sundaram04@users.noreply.github.com>
2023-09-22 22:10:09 +0530

    Create Program5.kt

---
+++ Program5.kt
@@ -0,0 +1,12 @@
+// Use Of Parameters And Arguments 
+
+fun birthdayGreeting(name: String): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+
+fun main() {
+    println(birthdayGreeting("Rover"))
+    println(birthdayGreeting("Rex"))
+}
