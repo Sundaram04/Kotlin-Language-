24140dcd3e8db71d5c66420435ff224c2817c4b5
Sundaram04 <145194021+Sundaram04@users.noreply.github.com>
2023-09-24 10:18:07 +0530

    Update myFirstAndroidApp.kt

--- myFirstAndroidApp.kt
+++ myFirstAndroidApp.kt
@@ -1,28 +1,42 @@
-package com.example.greetingcard
+package com.example.happybirthday
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.greetingcard.ui.theme.GreetingCardTheme
+import androidx.compose.ui.unit.sp
+import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+import androidx.compose.foundation.background
+import androidx.compose.ui.res.stringResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingCardTheme {
+            HappyBirthdayTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    GreetingImage(message = getString(R.string.happy_birthday_text), from = getString(
+                                            R.string.signature_text))
                 }
             }
         }
@@ -30,19 +44,55 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = Color.Cyan) {
-        Text(text = "Hi, my name is $name!",
-        modifier = modifier.padding(24.dp)
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    // Create a column so that texts don't overlap
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.CenterHorizontally)
+
         )
     }
-
 }
+@Composable()
+fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = message,
+            from = from,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(8.dp)
+        )
+    }
+    }
+
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    GreetingCardTheme {
-        Greeting("Sundaram")
+fun BirthdayCardPreview() {
+    HappyBirthdayTheme {
+        GreetingImage(stringResource(R.string.happy_birthday_text),
+            stringResource(R.string.signature_text))
     }
 }
